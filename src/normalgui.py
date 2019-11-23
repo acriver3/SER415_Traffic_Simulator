4f05b3b717dee970d6d755e138f399d0b7ad6172
acriver3 <acriver3@asu.edu>
2019-11-22 23:29:58 -0700

    Added the rest of normalgui

--- src/normalgui.py
+++ src/normalgui.py
@@ -22,9 +22,13 @@ t6 = tk.Text(top)
 t7 = tk.Text(top)
 t8 = tk.Text(top)
 
+b1 = tk.Button(top)
+
 l1 = tk.Label(top, image=main_bg)
 
-
+f1 = tk.Frame(top)
+f2 = tk.Frame(top)
+f3 = tk.Frame(top)
 
 t1.place(relx=0.041, rely=0.374, relheight=0.045, relwidth=0.06)
 t2.place(relx=0.041, rely=0.43, relheight=0.045, relwidth=0.06)
@@ -35,6 +39,12 @@ t6.place(relx=0.704, rely=0.411, relheight=0.045, relwidth=0.06)
 t7.place(relx=0.23, rely=0.019, relheight=0.045, relwidth=0.073)
 t8.place(relx=0.338, rely=0.019, relheight=0.045, relwidth=0.073)
 
+f1.place(relx=0.704, rely=0.093, relheight=0.215, relwidth=0.277)
+f2.place(relx=0.785, rely=0.411, relheight=0.215, relwidth=0.169)
+f3.place(relx=0.447, rely=0.019, relheight=0.084, relwidth=0.223)
+
 l1.place(relx=0.162, rely=0.112, height=341, width=371)
+
+b1.place(relx=0.054, rely=0.019, height=34, width=97)
 
 top.mainloop()
